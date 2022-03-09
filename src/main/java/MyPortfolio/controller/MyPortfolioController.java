1a9fc16080e4cf610b2f52654432261847774e26
Jihye Jeong <bananaqick@gmail.com>
2022-03-09 20:56:29 +0900

    test

--- src/main/java/MyPortfolio/controller/MyPortfolioController.java
+++ src/main/java/MyPortfolio/controller/MyPortfolioController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyPortfolioController {
-
-	@RequestMapping("/index.html")
-	public String indexGo() {
-		return "index";
-	}
 	
 	@RequestMapping("/myProfile")
 	public String myProfile() {
